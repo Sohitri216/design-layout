7bea1b584445637fac6d6eea22ae0f0de15ae7f0
sohitri <sohitri.sen@innofied.com>
2017-07-10 12:31:28 +0530

    modal close on escape and click on outside is done, README.md file added

---
+++ README.md
@@ -0,0 +1,6 @@
+# Design and Layout
+
+Frontend app for ui design layout for Quintype
+### instruction
+
+Browse the design-layout folder in your localhost
--- app.js
+++ app.js
@@ -1,29 +1,41 @@
-   'use strict';
-   document.addEventListener('DOMContentLoaded', function() {
-       /* global RvVanillaModal */
-       var modal = new RvVanillaModal({
-           showOverlay: true
-       });
+'use strict';
+document.addEventListener('DOMContentLoaded', function() {
+    /* global RvVanillaModal */
+    var modal = new RvVanillaModal({
+        showOverlay: true
+    });
 
-       // each method
-       modal.each(function(element) {
-           console.log('element:', element);
-           var target = element.getAttribute('data-rv-vanilla-modal');
-           console.log(target);
-           // var targetElement = document.querySelector(target);
-           var targetElement = document.querySelector('#target-modal');
-           var closeBtn = targetElement.querySelector(modal.settings.closeSelector);
+    // each method
+    modal.each(function(element) {
+        console.log('element:', element);
+        var target = element.getAttribute('data-rv-vanilla-modal');
+        console.log(target);
+        // var targetElement = document.querySelector(target);
+        var targetElement = document.querySelector('#target-modal');
+        var closeBtn = targetElement.querySelector(modal.settings.closeSelector);
 
-           // close click listerner
-           closeBtn.addEventListener('click', function(event) {
-               event.preventDefault();
-               modal.close(targetElement);
-           });
+        // close click listener
+        closeBtn.addEventListener('click', function(event) {
+            event.preventDefault();
+            modal.close(targetElement);
+        });
 
-           // open click listerner
-           element.addEventListener('click', function(event) {
-               event.preventDefault();
-               modal.open(targetElement);
-           });
-       });
-   }, false);
+        // open click listener
+        element.addEventListener('click', function(event) {
+            event.preventDefault();
+            modal.open(targetElement);
+        });
+
+        $(document).keyup(hideModal);
+        $('.rv-vanilla-modal-overlay').click(hideModal);
+
+        // hide modal on esc key press and click on overlay
+        function hideModal(e) {
+            if (e.keyCode === 27 || e.type === 'click') {
+                event.preventDefault();
+                modal.close(targetElement);
+            }
+        }
+
+    });
+}, false);
